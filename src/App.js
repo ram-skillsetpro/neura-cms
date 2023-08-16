2cfef8b25d8051f7c7f63737e3a99170e5ddb699
Prasoon <gautam.gr8@gmail.com>
2023-08-17 01:01:39 +0530

    ui fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { styled } from '@mui/material/styles';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavigationBar } from './components/NavigationBar';
 import { User } from './pages/User';
@@ -9,21 +9,34 @@ import { Login } from './pages/Login';
 import { Index } from './pages/Index';
 import { NoMatch } from './NoMatch';
 import Sidebar from './components/Sidebar';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
 
 function App() {
   return (
     <React.Fragment>
       <Router>
-        <NavigationBar />
-
+           <NavigationBar />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <div>
+            <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+        <Card sx={{ minWidth: "100%" }}>
         <Sidebar />
-
-        <Switch>
-          <Route exact path="/" component={Index} />
-          <Route path="/user" component={User} />
-          <Route path="/login" component={Login} />
-          <Route component={NoMatch} />
-        </Switch>
+        </Card>
+        </Grid>
+        <Grid item xs={9}>
+        <Route exact path="/" component={Index} />
+                  <Route path="/user" component={User} />
+                  <Route component={NoMatch} />
+        </Grid>
+      </Grid>
+    </Box>                 
+          </div>
+          </Switch>
       </Router>
     </React.Fragment>
   );
