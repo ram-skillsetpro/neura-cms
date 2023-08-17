10a5818a707d5e21b627a45150ecdda06dc24c40
Mohd Yasin <mohd.yasin@timesinternet.in>
2023-08-17 07:58:50 +0530

    import fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,6 +10,7 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import fetcher from '../utils/fetcher'
 
 function Login() {
 
