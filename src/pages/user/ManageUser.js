907ad89ab303727218fcf5ec552fa828f2ae870a
Mohd Yasin <mohd.yasin@timesinternet.in>
2023-08-22 23:05:21 +0530

    update

--- src/pages/user/ManageUser.js
+++ src/pages/user/ManageUser.js
@@ -95,22 +95,10 @@ const ManageUser = () => {
     setOpenStates(newOpenStates);
   };
 
-
-
-  const handleEditUser = (user, index) => {
-    handleMenuClose(index);
-    setUser(user);
-  }
-
   const handleOpenUserCompanyMapDialog = (user, index) => {
     handleMenuClose(index);
     setUser(user);
     setOpenUserCompanyMapDialog(true);
-  };
-
-  const handleCloseCompanyDialog = () => {
-    setUser(null);
-    fetchUserList(currentPage);
   };
 
   const handlePageChange = (event, page) => {
@@ -196,7 +184,6 @@ const ManageUser = () => {
                     'aria-labelledby': `basic-button-${index}`
                   }}
                 >
-                  <MenuItem onClick={() => handleEditUser(user, index)}>Edit</MenuItem>
                   <MenuItem onClick={() => handleOpenUserCompanyMapDialog(user, index)}>Activate</MenuItem>
                 </Menu>
               </TableCell>
