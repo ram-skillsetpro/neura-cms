df52b17d40a6680dedc3dd8a1ba97bdec0a7dd6c
Ramesh Kumawat <rk.kumawat123@gmail.com>
2023-10-03 22:22:37 +0530

    project structure modified , header issue fixed in login page

--- src/App.js
+++ src/App.js
@@ -1,52 +1,22 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { isAuthenticated, isRestrictedRoutWithAuthority, canAccessTheRouteWithUserAuthorities } from './utils/authGuard';
+import { BrowserRouter } from 'react-router-dom'; 
 import { NavigationBar } from './components/NavigationBar';
-import Layout from './components/layout/Layout'; // Import the Layout HOC
-import Login from './pages/Login';
-import { Index } from './pages/Index';
-import { NoMatch } from './pages/NoMatch';
-import ManageUser from './pages/user/ManageUser';
-import ManageCompany from './pages/company/ManageCompany';
-import CreateCompany from './pages/company/Create';
-import CreateContract from './pages/contract/Create';
-import ManageContract from './pages/contract/ManageContract';
-import CreateClause from './pages/clause/CreateClause';
-import ManageClause from './pages/clause/ManageClause';
-import ClausePrompt from './pages/clause/ClausePrompt';
-import AddClausePrompt from './pages/clause/AddClausePrompt';
-
-function PrivateRoute({ element }) {
-  let isAuthentic = isAuthenticated();
-  const pathname = useLocation().pathname;
-  if (isRestrictedRoutWithAuthority(pathname) && !canAccessTheRouteWithUserAuthorities(pathname)) {
-    isAuthentic = false;
-  }
-  return isAuthentic ? element : <Navigate to="/login" />;
-}
+import Routers from './Routers/Routers';
+import { isAuthenticated } from './utils/authGuard';
 
 function App() {
+  const isAuthentic = isAuthenticated();
+
   return (
-    <Router>
-      <React.Fragment>
-        <NavigationBar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<PrivateRoute element={<Layout> <Index /> </Layout>} />} />
-          <Route path="/company/create" element={<PrivateRoute element={<Layout> <CreateCompany /> </Layout>} />} />
-          <Route path="/company/manage" element={<PrivateRoute element={<Layout> <ManageCompany /> </Layout>} />} />
-          <Route path="/user/manage" element={<PrivateRoute element={<Layout> <ManageUser /> </Layout>} />} />
-          <Route path="/contract/create" element={<PrivateRoute element={<Layout> <CreateContract /> </Layout>} />} />
-          <Route path="/contract/manage" element={<PrivateRoute element={<Layout> <ManageContract /> </Layout>} />} />
-          <Route path="/clause/create" element={<PrivateRoute element={<Layout> <CreateClause /> </Layout>} />} />
-          <Route path="/clause/manage" element={<PrivateRoute element={<Layout> <ManageClause /> </Layout>} />} />
-          <Route path="/clause-prompt/manage" element={<PrivateRoute element={<Layout> <ClausePrompt /> </Layout>} />} />
-          <Route path="/clause-prompt/add" element={<PrivateRoute element={<Layout> <AddClausePrompt /> </Layout>} />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
-      </React.Fragment>
-    </Router>
+    <BrowserRouter>
+      <>
+        {isAuthentic ? 
+          <NavigationBar /> 
+          : null
+        }
+        <Routers />
+      </>
+    </BrowserRouter>
   );
 }
 
