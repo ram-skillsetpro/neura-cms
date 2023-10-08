bb17a922237e1efd52f0908f185e7b91579236d2
Mohd Yasin <mohd.yasin@timesinternet.in>
2023-10-08 10:30:21 +0530

    header and logout issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'; 
-import Header from './components/layout/header/Header';
 import Routers from './routers/Routers';
-import { isAuthenticated } from './utils/authGuard';
 
 function App() {
-  const isAuthentic = isAuthenticated();
-
   return (
     <BrowserRouter>
       <Routers />
