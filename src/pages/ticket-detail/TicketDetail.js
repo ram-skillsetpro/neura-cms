3ab5366977d92c1bba1c0e37600575f08a9a712e
Mohd Yasin <mohd.yasin@timesinternet.in>
2023-11-21 22:53:16 +0530

    ticket details page api integration

--- src/pages/ticket-detail/TicketDetail.js
+++ src/pages/ticket-detail/TicketDetail.js
@@ -9,23 +9,140 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import style from './TicketDetail.module.scss';
 import { useSearchParams } from 'react-router-dom';
 import fetcher from '../../utils/fetcher';
+import { hasAuthority } from '../../utils/authGuard';
+import { AUTHORITY } from "../../utils/constants";
 
 const TicketDetail = () => {
     const [searchParams, setSearchParams] = useSearchParams()
     const [expanded, setExpanded] = React.useState(false);
+    const [deProcessedMeta, setDeProcessedMeta] = React.useState([]);
+    const [updatedData, setUpdatedData] = React.useState(null);
+
 
     const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
+        setExpanded((prevExpanded) => ({
+          ...prevExpanded,
+          [panel]: isExpanded ? panel : false,
+        }));
+    };
+
+    const handleInputChange1 = (sectionIndex, itemIndex, key, newValue) => {
+        const updatedState = [...deProcessedMeta];
+        updatedState[sectionIndex].value[itemIndex].value[key] = newValue;
+        setDeProcessedMeta(updatedState);
+        console.log(updatedState);
+    };
+
+    const handleInputChange = (sectionIndex, itemIndex, key, newValue) => {
+        const updatedState = JSON.parse(JSON.stringify(deProcessedMeta));
+        const [subSelection, subIndex] = sectionIndex.toString().split('_');
+        if (subIndex) {
+            let currentItem = updatedState[subSelection].value[subIndex];
+            currentItem.value = currentItem.value.map((subItem) => {
+                if (subItem.key === key) {
+                  return { key, value: newValue };
+                } else {
+                  return subItem;
+                }
+              });
+        } else {
+          let currentItem = updatedState[sectionIndex].value[itemIndex];
+          currentItem = {
+            value: newValue,
+            key: currentItem.key,
+          };
+          updatedState[sectionIndex].value[itemIndex] = currentItem;
+        }
+        setDeProcessedMeta(updatedState);
+        console.log(JSON.stringify(updatedState));
     };
 
     const openFile = async (id) => {
         try {
           const res = await fetcher.post(`deqc/open-assign?fileId=${id}`);
-          console.log(res);
+          setDeProcessedMeta(JSON.parse(res.response.deProcessedMeta));
+          setUpdatedData(deProcessedMeta);
         } catch (error) {
           console.log(error);
         }
-      };
+    };
+
+    const saveInboxItem = async (status) => {
+        try {
+            const payload = {
+                id: searchParams.get('id'),  
+                deProcessedMeta: JSON.stringify(deProcessedMeta),
+                status: status
+            }
+            const res = await fetcher.post(`deqc/save-inbox-item`, payload);
+            console.log(res);
+        } catch (error) {
+          console.log(error);
+        }
+    };
+
+    const disableInput = (status) => {
+        if (hasAuthority(AUTHORITY.USER_DE)) {
+            return status === 2 || status === 6;
+        } else {
+            return status === 4 || status === 7;
+        }
+    };
+
+    const renderAccordionContent = (data, parentIndex, status) => {
+        return (
+          <div>
+            {data.map((item, itemIndex) => (
+              <div key={itemIndex} className='form-group'>
+                {typeof item.value === 'object' ? (
+                  // If the value is an object, render nested accordions
+                  <Accordion
+                    expanded={expanded[`panel${parentIndex}_${itemIndex}`]}
+                    onChange={handleChange(`panel${parentIndex}_${itemIndex}`)}
+                  >
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls={`panel${parentIndex}_${itemIndex}bh-content`}
+                      id={`panel${parentIndex}_${itemIndex}bh-header`}
+                    >
+                      <Typography>{item.key}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      {renderAccordionContent(item.value, `${parentIndex}_${itemIndex}`, status)}
+                    </AccordionDetails>
+                  </Accordion>
+                ) : (
+                  // If the value is not an object, render input field
+                  <>
+                    <div className='form-group'>
+                        <label>{item.key}</label>
+                        <input
+                            name={item.key}
+                            type="text"
+                            className="form-control"
+                            value={item.value}
+                            onChange={(e) =>
+                                handleInputChange(parentIndex, itemIndex, item.key, e.target.value)
+                            }
+                            disabled={disableInput(status)}
+                        />
+                    </div>
+                  </>
+                )}
+              </div>
+            ))}
+          </div>
+        );
+    };
+
+    const handleMetaAction = (index, status) => {
+        const updatedState = JSON.parse(JSON.stringify(deProcessedMeta));
+        const section = updatedState[index];
+        section.status = status;
+        updatedState[index] = section;
+        setDeProcessedMeta(updatedState);
+        //console.log(updatedState);
+    };
 
     useEffect(() => {
         openFile(searchParams.get('id'));
@@ -49,118 +166,56 @@ const TicketDetail = () => {
                     className={style.ticketPDF}> 
                 </object> 
 
-                <section className={style.ticketDetailForm}>
-                    <div className={style.accBox}>
-                        <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1bh-content"
-                                id="panel1bh-header"
-                            >
-                                <Typography sx={{ width: 'calc(95% - 24px)', fontSize: '14px', flexShrink: 0 }}>
-                                    Effective Date
-                                </Typography>
-                                <TaskAltIcon color="success" />
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <div className='form-group'>
-                                    <input
-                                        name="effectiveDate" 
-                                        type="text"
-                                        className="form-control"
-                                        value="13-2-2023"
-                                        disabled
-                                    />
-                                </div>
-                                
-                                <div className={style.ticActionBtn}>
-                                    {/* QA Action Button */}
-                                    {/* <button className='btn btn-success'>Verify</button>  */}
-
-                                    {/* DE Action Button */}
-                                    {/* <button className='btn btn-success'>Ok</button> */}
-                                    <button className='btn btn-secondary'>Edit</button>
-                                    <button className='btn btn-primary'>Skip</button> 
-                                </div>
-                            </AccordionDetails>
-                        </Accordion>
-
-                        <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-                            <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel2bh-content"
-                            id="panel2bh-header"
-                            > 
-                                <Typography sx={{ width: 'calc(95% - 24px)', fontSize: '14px', flexShrink: 0 }}>
-                                    Parties Detail
-                                </Typography>
-                                {/* <TaskAltIcon color="success" /> */}
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                
-                                <div className='form-group'>
-                                    <label className='label-control'>First Party </label>
-                                    <input
-                                        name="firtParty" 
-                                        type="text"
-                                        className="form-control" 
-                                        value="Delhivery Pvt Ltd"
-                                    />
-                                </div>
-                                <div className='form-group'>
-                                    <label className='label-control'>Second Party</label>
-                                    <input
-                                        name="secondParty" 
-                                        type="text"
-                                        className="form-control" 
-                                        value="Twishan International Pvt Ltd"
-                                    />
-                                </div>
-                                
-                                <div className={style.ticActionBtn}>
-                                    {/* QA Action Button */}
-                                    {/* <button className='btn btn-success'>Verify</button>  */}
-
-                                    {/* DE Action Button */}
-                                    <button className='btn btn-success'>Ok</button>
-                                    <button className='btn btn-secondary'>Edit</button>
-                                    <button className='btn btn-primary'>Skip</button> 
-                                </div>
-                            </AccordionDetails>
-                        </Accordion>
-                        <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-                            <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel3bh-content"
-                            id="panel3bh-header"
-                            >
-                                <Typography sx={{ width: 'calc(95% - 24px)', fontSize: '14px', flexShrink: 0 }}>
-                                    Indemnity Clause
-                                </Typography>
-                                {/* <TaskAltIcon color="success" /> */}
-                            </AccordionSummary>
-                            <AccordionDetails> 
-                                <div className='form-group'> 
-                                    <textarea className="form-control h_120"></textarea>
-                                </div>
-                                
-                                <div className={style.ticActionBtn}>
-                                    {/* QA Action Button */}
-                                    {/* <button className='btn btn-success'>Verify</button>  */}
-
-                                    {/* DE Action Button */}
-                                    <button className='btn btn-success'>Ok</button>
-                                    <button className='btn btn-secondary'>Edit</button>
-                                    <button className='btn btn-primary'>Skip</button> 
-                                </div>
-                            </AccordionDetails>
-                        </Accordion> 
-                    </div>
-
-                    <div className='text-center'>
-                        <button className='btn btn-primary' disabled>Update All</button>
-                    </div>
-                </section>
+                {deProcessedMeta.length > 0 && (
+                    <section className={style.ticketDetailForm}>
+                        <div className={style.accBox}>
+                            {deProcessedMeta && deProcessedMeta.map((section, index) => (
+                                <Accordion
+                                    key={index}
+                                    expanded={expanded[`panel${index}`]}
+                                    onChange={handleChange(`panel${index}`)}
+                                >
+                                    <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls={`panel${index}bh-content`}
+                                    id={`panel${index}bh-header`}
+                                    >
+                                    <Typography>{section.key}</Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                        {renderAccordionContent(section.value, index, section.status)}
+                                        {hasAuthority(AUTHORITY.USER_DE) ? (
+                                            <div className={style.ticActionBtn}>
+                                                <button className='btn btn-success' onClick={() => handleMetaAction(index, 2)}>Ok</button>
+                                                <button className='btn btn-secondary' onClick={() => handleMetaAction(index, 0)}>Edit</button>
+                                                <button className='btn btn-primary' onClick={() => handleMetaAction(index, 6)}>Skip</button> 
+                                            </div>
+                                        ) : (
+                                            <div className={style.ticActionBtn}>
+                                                <button className='btn btn-success' onClick={() => handleMetaAction(index, 4)}>Ok</button>
+                                                <button className='btn btn-secondary' onClick={() => handleMetaAction(index, 0)}>Edit</button>
+                                                <button className='btn btn-primary' onClick={() => handleMetaAction(index, 7)}>Skip</button> 
+                                            </div>
+                                        )}
+                                    </AccordionDetails>
+                                </Accordion>
+                            ))}
+                        </div>
+                        {hasAuthority(AUTHORITY.USER_DE) ? (
+                            <div className='text-center'>
+                                <button className='btn btn-primary' onClick={() => saveInboxItem(5)}>Save</button>
+                                <button className='btn btn-primary' onClick={() => saveInboxItem(6)}
+                                    disabled={!deProcessedMeta.every(section => section.status === 2 || section.status === 6)}>Submit</button>
+                            </div>
+                        ) : (
+                            <div className='text-center'>
+                                <button className='btn btn-primary' onClick={() => saveInboxItem(7)}>Save</button>
+                                <button className='btn btn-primary' onClick={() => saveInboxItem(8)}
+                                    disabled={!deProcessedMeta.every(section => section.status === 4 || section.status === 7)}>Submit</button>
+                            </div>
+                        )}
+                    </section>
+                )}
             </div>
         </>
     )
