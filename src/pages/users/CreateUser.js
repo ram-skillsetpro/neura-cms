c4691b92f3d88cc5d2c0bb652c2d21fe102e0c31
Mohd Yasin <mohd.yasin@timesinternet.in>
2024-01-25 13:07:18 +0530

    add user

--- src/pages/users/CreateUser.js
+++ src/pages/users/CreateUser.js
@@ -1,184 +1,186 @@
 import React, { useEffect, useState } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
-import { FormControlLabel, IconButton, Radio, RadioGroup } from '@mui/material';
+import { CircularProgress, FormControlLabel, IconButton, Radio, RadioGroup } from '@mui/material';
 import style from './Users.module.scss';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import SnackBar from '../../components/SnackBar';
+import fetcher from '../../utils/fetcher';
 
-const CreateUser = ({closeEvent, user}) => {  
+const CreateUser = ({closeEvent}) => {  
+
+    const [progress, setProgress] = useState(false);
+    const [countryList, setCountryList] = useState([]);
+
+    const [snackbar, setSnackbar] = useState({
+        show: false,
+        status: "",
+        message: "",
+      });
+    const toggleSnackbar = (value) => {
+        setSnackbar(value);
+    };
+
+    const [formData, setFormData] = useState({
+        countryCode: '+91',
+        email: '',
+        name: '',
+        roleId: '',
+        phone: '',
+        status: true
+    });
+
+    const handleSnackMsg = (res) => {
+        setSnackbar({
+            show: true,
+            status: res?.status === 200 ? 'success' : 'error',
+            message: res?.status === 200 ? 'User created successfully' : (res?.response || res?.message)
+        });
+    };
+    const handleSubmit = async (values) => {
+        try {
+            setProgress(true);
+            const res = await fetcher.post('cms/create-user', values);
+            handleSnackMsg(res);
+            if (res.status === 200) {
+                setTimeout(() => {
+                    closeEvent();
+                }, 3000);
+            }
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setProgress(false);
+        }
+    };
+
+    const validationSchema = Yup.object().shape({
+        countryCode: Yup.string().required('Name is required'),
+        email: Yup.string().email('Invalid email address').required('Email is required'),
+        phone: Yup.string().required('Phone is required'),
+        firstName: Yup.string().required('Company is required'),
+        roleId: Yup.string().required('User type is required')
+    });
+    
+    const formik = useFormik({
+        initialValues: formData,
+        validationSchema: validationSchema,
+        onSubmit: handleSubmit,
+    });
+
+    const fetchCountryList = async () => {
+        const res = await fetcher.get(`country-list`);
+        setCountryList(res.response);
+    }
+
+    useEffect(() => {
+        fetchCountryList();
+    }, []);
+
     return(
         <>
             <div className="createMainTitle">
-                <h2>{user ? 'Edit User' : 'Create User'}</h2>
+                <h2>Create User</h2>
                 <IconButton onClick={closeEvent}>
                     <CloseIcon />
                 </IconButton>
             </div>
 
-            <form>
+            {progress ? <CircularProgress /> : null}
+            <form onSubmit={formik.handleSubmit}>
                 <div className="createSection mb-3"> 
-                    <h3 className="createSubTitle">General</h3 > 
                     
                     <section className="createFormSection">
                         <div className='row'>
                             <div className='col-md-6 form-group'>
-                                <label className='label-control'>Username<span>*</span></label>
+                                <label className='label-control'>Name<span>*</span></label>
                                 <input
-                                    name="username"
+                                    name="firstName"
+                                    onChange={formik.handleChange}
+                                    value={formik.values.firstName}
                                     type="text"
                                     className="form-control"
                                 />
-                                {/* <div className='errorMsg'>Error msg here...</div> */}
+                                { formik.touched.firstName && formik.errors.firstName && (
+                                    <div className='errorMsg'>{formik.errors.firstName}</div>
+                                )}
                             </div>
 
                             <div className='col-md-6 form-group'>
                                 <label className='label-control'>Email<span>*</span></label>
                                 <input
                                     name="email"
+                                    onChange={formik.handleChange}
+                                    value={formik.values.email}
                                     type="text"
                                     className="form-control"
                                 />
-                                {/* <div className='errorMsg'>Error msg here...</div> */}
+                                { formik.touched.email && formik.errors.email && (
+                                    <div className='errorMsg'>{formik.errors.email}</div>
+                                )}
                             </div>
 
                             <div className='col-md-6 form-group'>
                                 <label className='label-control'>Phone No.</label>
                                 <div className='row no-gutters phoneNumberField'>
                                     <div className="col w65">
-                                        <select className='form-control px-2'>
-                                            <option>+91</option>
-                                        </select>
+                                    <select
+                                                name="countryCode"
+                                                onChange={formik.handleChange}
+                                                value={formik.values.countryCode}
+                                                className='form-control px-2'
+                                            >
+                                                {countryList.map((country, index) => (
+                                                    <option key={index} value={country.country_code}>
+                                                        {country.country_code}
+                                                    </option>
+                                                ))}
+                                            </select>
                                     </div>
                                     <div className="col">
                                         <input
-                                            name="phone" 
+                                            name="phone"
+                                            onChange={formik.handleChange}
+                                            value={formik.values.phone}
                                             type="text"
                                             className="form-control"
-                                        /> 
-                                        {/* <div className='errorMsg'>Error msg here...</div> */}
+                                        />
+                                        { formik.touched.phone && formik.errors.phone && (
+                                            <div className='errorMsg'>{formik.errors.phone}</div>
+                                        )}
                                     </div>
                                 </div> 
                             </div>
 
                             <div className='col-md-6 form-group'>
-                                <label className='label-control'>Password<span>*</span></label>
-                                <input
-                                    name="password"
-                                    type="password"
-                                    className="form-control"
-                                />
-                                <div className='row mt-1 align-items-center'>
-                                    <div className="col-6">
-                                        {/* <div className={`${style.passProgress} ${style.weekPass}`}>Week</div>
-                                        <div className={`${style.passProgress} ${style.medPass}`}>Medium</div> */}
-                                        <div className={`${style.passProgress} ${style.strongPass}`}>Strong</div>
-                                    </div>
-                                    <div className="col-6 text-right">
-                                        <button className={style.generateBtn}>Generate</button>
-                                    </div>
-                                </div>
-                                {/* <div className='errorMsg'>Error msg here...</div> */}
-                            </div>
-
-                            <div className='col-md-12 form-group m-0'>
-                                <label className='label-control'>User Status</label> 
-                                <RadioGroup
-                                    row
-                                    aria-labelledby="demo-row-radio-buttons-group-label"
-                                    name="status" 
-                                >
-                                    <FormControlLabel
-                                        value="1"
-                                        control={<Radio sx={{'& .MuiSvgIcon-root': { fontSize: 18, }}} />}
-                                        label="Active"
-                                    />
-                                    <FormControlLabel
-                                        value="0"
-                                        control={<Radio sx={{'& .MuiSvgIcon-root': { fontSize: 18, }}} />}
-                                        label="Inactive"
-                                    />
-                                </RadioGroup>
+                                <label className='label-control'>User Type</label>
+                                <select
+                                    className='form-control'
+                                    name='roleId'
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.roleId}
+                                    >
+                                    <option value='' label='Select user type' />
+                                    <option value='7' label='Legal DE' />
+                                    <option value='6 Manager' label='Legal QC' />
+                                    <option value='9' label='Sales Ops' />
+                                    <option value='10' label='Legal Ops' />
+                                </select>
+                                { formik.touched.roleId && formik.errors.roleId && (
+                                    <div className='errorMsg'>{formik.errors.roleId}</div>
+                                )}
                             </div>
                         </div>
                     </section>
                 </div>
                 
-                <div className="createSection mb-3"> 
-                    <h3 className="createSubTitle">Other Details</h3> 
-                    
-                    <section className="createFormSection">
-                        <div className='row'>
-                            <div className='col-md-6 form-group'>
-                                <label className='label-control'>User Type</label>
-                                <select className='form-control'>
-                                    <option>QE</option>
-                                    <option>QE Manager</option>
-                                    <option>Sales</option>
-                                    <option>Demo Manager</option>
-                                </select> 
-                            </div>
-
-                            <div className='col-md-6 form-group'>
-                                <label className='label-control'>Department</label>
-                                <select className='form-control'>
-                                    <option>Product</option> 
-                                    <option>Sales</option>
-                                </select> 
-                            </div>
-
-                            <div className='col-md-6 form-group'>
-                                <label className='label-control'>Login Type</label>
-                                <select className='form-control'>
-                                    <option>Option 1</option> 
-                                    <option>Option 2</option> 
-                                </select> 
-                            </div>
-
-                            <div className='col-md-6 form-group'>
-                                <label className='label-control'>Login Expiry</label>
-                                Datepicker here...
-                            </div>
-                            
-                            <div className='col-md-6 form-group'>
-                                <label className='label-control'>Two Factor Auth</label>
-                                <select className='form-control'>
-                                    <option>Option 1</option> 
-                                    <option>Option 2</option> 
-                                </select> 
-                            </div>
-
-                            <div className='col-md-6 form-group'>
-                                <label className='label-control'>Login Status</label>
-                                <select className='form-control'>
-                                    <option>Option 1</option> 
-                                    <option>Option 2</option> 
-                                </select> 
-                            </div>
-                            
-                            <div className='col-md-6 form-group'>
-                                <label className='label-control'>OnRoll/Vendor</label>
-                                <select className='form-control'>
-                                    <option>Option 1</option> 
-                                    <option>Option 2</option> 
-                                </select> 
-                            </div>
-
-                            <div className='col-md-6 form-group'>
-                                <label className='label-control'>Vendor Name</label>
-                                <input
-                                    name="vendorName"
-                                    type="text"
-                                    className="form-control"
-                                />
-                            </div> 
-                        </div>
-                    </section>
-                </div>
-
                 <div className='d-flex justify-content-end'>
-                    {/* <button className='btn btn-danger mr-auto'>Delete User</button> */}
-                    <button className='btn btn-outline-primary mr-2'>Reset</button>
-                    <button type="submit" className='btn btn-primary'>Save User</button>
+                    <button className='btn btn-outline-primary mr-2' onClick={closeEvent}>Cancel</button>
+                    <button type="submit" className='btn btn-primary' disabled={progress}>Save User</button>
                 </div>
             </form>
+            <SnackBar {...snackbar} onClose={toggleSnackbar} />
             
         </>
     )
